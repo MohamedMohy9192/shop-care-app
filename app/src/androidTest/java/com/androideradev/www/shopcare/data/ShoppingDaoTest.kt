c3d67bb89f4e581281dcdfa35f9c23b1f6d0fb7f
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2022-04-30 20:09:47 +0200

    Create ShoppingDao test case

--- app/src/androidTest/java/com/androideradev/www/shopcare/data/ShoppingDaoTest.kt
+++ app/src/androidTest/java/com/androideradev/www/shopcare/data/ShoppingDaoTest.kt
@@ -1,11 +1,60 @@
 package com.androideradev.www.shopcare.data
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.junit.Assert.*
 
 @RunWith(AndroidJUnit4::class)
 @SmallTest
 class ShoppingDaoTest {
-    
+
+    // This is a JUnit test rule that swaps the background executor used
+    // by the architecture components with a different one that executes each task synchronously.
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var database: ShoppingItemDatabase
+    private lateinit var dao: ShoppingDao
+
+    @Before
+    fun setup() {
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            ShoppingItemDatabase::class.java
+        ).allowMainThreadQueries().build()
+
+        dao = database.shoppingDao()
+    }
+
+    @After
+    fun teardown() {
+        database.close()
+    }
+
+    @Test
+    fun insertShoppingItem() = runTest {
+        val shoppingItem = ShoppingItem(
+            id = 1,
+            name = "Phone",
+            amount = 1,
+            price = 1000f,
+            imageUrl = "www.example.com"
+        )gi
+        dao.insertShoppingItem(shoppingItem)
+        dao.getAllShoppingItems().observeForever { shoppingItems: List<ShoppingItem> ->
+            assertTrue(shoppingItems.contains(shoppingItem))
+
+        }
+    }
+
+
 }
